a1f081ae290b60275afc25c63535b16b91ee3ced
q57561097 <929248901@qq.com>
2017-01-03 11:01:31 +0800

    555

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from mysite import settings
 
 
 class UserProfile(models.Model):
@@ -10,10 +11,20 @@ class UserProfile(models.Model):
     phone = models.CharField(max_length=16, default='', blank=True)
 
     def __str__(self):
-        return self.nickname
+        return self.user.username
 
-    def __unicode__(self):
-        return self.nickname
+    class Meta:
+        db_table = 'UserProfile'
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            try:
+                p = UserProfile.objects.get(user=self.user)
+                self.pk = p.pk
+            except UserProfile.DoesNotExist:
+                pass
+
+        super(UserProfile, self).save(*args, **kwargs)
 
 
 def create_user_profile(sender, instance, created, **kwargs):
